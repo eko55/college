33739c19d35b85b52780e8779eb285ff4eefbad6
hp <stdgt4@gmail.com>
2024-04-07 15:40:54 +0300

    Add project structure

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,56 @@
+CREATE TABLE faculties (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) UNIQUE,
+	address VARCHAR(100),
+	phone VARCHAR(20),
+	email VARCHAR(50)
+);
+
+CREATE TABLE departments (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) UNIQUE,
+	phone VARCHAR(20),
+	email VARCHAR(50),
+	faculty_id INT,
+	FOREIGN KEY (faculty_id) REFERENCES faculties(id)
+);
+
+CREATE TABLE teachers (
+	id SERIAL PRIMARY KEY,
+	personal_number CHAR(10) UNIQUE,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	department_id INT,
+	FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE courses (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) UNIQUE,
+	description TEXT,
+	teacher_id INT,
+	FOREIGN KEY (teacher_id) REFERENCES teachers(id)
+);
+
+CREATE TABLE students (
+	id SERIAL PRIMARY KEY,
+	faculty_number VARCHAR(10) UNIQUE,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50)
+);
+
+CREATE TABLE courses_registrations (
+	id SERIAL PRIMARY KEY,
+	course_id INT,
+	student_id INT,
+	FOREIGN KEY (course_id) REFERENCES courses(id),
+	FOREIGN KEY (student_id) REFERENCES students(id)
+);
+
+CREATE TABLE head_of_department (
+	id SERIAL PRIMARY KEY,
+	teacher_id INT UNIQUE,
+	department_id INT UNIQUE,
+	FOREIGN KEY (teacher_id) REFERENCES teachers(id),
+	FOREIGN KEY (department_id) REFERENCES departments(id)
+);
